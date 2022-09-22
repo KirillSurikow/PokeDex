62eb781d3dd4807ff4c21c31284a22326f73e38f
Kirill Surikow <K.Surikow94@gmx.de>
2022-09-22 13:31:20 +0200

    update when scroll

--- script.js
+++ script.js
@@ -2,19 +2,41 @@ let pokeLexikon = [];
 let currentPokemon;
 let loadLimit = 31;
 let offset = 1;
-let pokedex = document.getElementById('pokedexContainer');
+
 
 async function loadPokemons() {
-   for (let i = offset; i < loadLimit; i++){ 
-    let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    let response = await fetch(url);
-    let currentPokemon = await response.json();
-    pokeLexikon.push(currentPokemon);
-    renderPokemonCards(i);
-}
+    for (let i = offset; i < loadLimit; i++) {
+        let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        let response = await fetch(url);
+        let currentPokemon = await response.json();
+        pokeLexikon.push(currentPokemon);
+        renderPokemonCards(i);
+    }
 }
 
-async function renderPokemonCards(i){
-    let pokemonName =  pokeLexikon[i]['name'];
-    pokedex.innerHTML += /*html*/`<div>${pokemonName}</div>`
+async function renderPokemonCards(i) {
+    let pokedex = document.getElementById('pokedex');
+    let pokemonName = pokeLexikon[i - 1]['name'];
+    let pokemonImg = pokeLexikon[i-1]['sprites']['other']['official-artwork']['front_default'];
+    pokedex.innerHTML += /*html*/`
+        <div class="pokeCard">
+          <div class="pokemonID">#${i}</div>  
+          <div class="pokemonName">${pokemonName}</div>
+          <div class="pokemonImg"><img src="${pokemonImg}"></div>
+        </div>
+        `
+}
+
+let loadMorePokemons = async () => {
+    if (window.scrollY + window.innerHeight >= document.body.clientHeight){
+        for (let i = loadLimit; i < loadLimit + 31 ; i++) {
+            let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+            let response = await fetch(url);
+            let currentPokemon = await response.json();
+            pokeLexikon.push(currentPokemon);
+            renderPokemonCards(i);
+        }
+        loadLimit +=30;
+        offset += 30;
+    }
 }
--- templates.js
+++
@@ -1,3 +0,0 @@
-function renderPokemonCards(currentPokemon, i){
-    
-}
