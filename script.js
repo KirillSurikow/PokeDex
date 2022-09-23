879f53c5970a7ec0ffc3146d1993927f16da7278
Kirill Surikow <K.Surikow94@gmx.de>
2022-09-23 11:47:53 +0200

    detail Interface head Infos

--- script.js
+++ script.js
@@ -49,3 +49,82 @@ let loadMorePokemons = async () => {
         offset += 30;
     }
 }
+
+function showDetails(i){
+   showDetailInterface();
+   hideAndDisable();
+   showDetailInterfaceNav(i);
+   loadPokemonSingle(i);
+   loadPokemonTypeSingle(i)
+   loadPokemonDetailsSingle(i);
+}
+
+function showDetailInterface(){
+  document.getElementById('detailInterface').classList.remove('d-none');
+}
+
+function hideAndDisable(){
+    document.getElementById('pokedex').classList.add('hideAndDisable');
+    document.getElementById('header').classList.add('hideAndDisable');
+}
+
+function showDetailInterfaceNav(i){
+    let detailInterface = document.getElementById('detailInterface');
+    detailInterface.innerHTML += /*html*/`
+       <div class="detailNav"> 
+          <div class="closeDetailIntervace">
+             <img class="closeBtn" src="img/close.png" alt="close">
+          </div>
+          <div class="nextPrev">
+             <img class="previousBtn" src="img/previous.png" alt="previous">
+             <img class="nextBtn" src="img/next.png" alt="next">
+          </div>
+       </div>
+        `
+}
+
+function loadPokemonSingle(i){
+    let pokemonName = pokeLexikon[i - 1]['name'];
+    let pokemonImg = pokeLexikon[i - 1]['sprites']['other']['official-artwork']['front_default']; 
+    let pokemonWeight = pokeLexikon [i - 1]['height'];
+    let pokemontHeight = pokeLexikon [i - 1]['weight'];
+    showPokemonSingle(i, pokemonName, pokemonImg, pokemonWeight, pokemontHeight);
+}
+
+function loadPokemonTypeSingle(i){
+    let characteristicsOfPokemon = pokeLexikon[i-1]['types']
+    for (j = 0; j < characteristicsOfPokemon.length; j++) {
+      let type =  characteristicsOfPokemon[j]['type']['name'];
+      showTypesSingle(i, type);
+    }
+}
+
+function loadPokemonDetailsSingle(i){
+    let pokemonStats = pokeLexikon[i-1]['stats'];
+    for (j = 0; j < pokemonStats.length; j++){
+        let statName = pokemonStats[j]['stat']['name'];
+        let statNumber = pokemonStats[j]['base_stat'];
+        showPokemonDetailsSingle(i, statName, statNumber)
+    }
+}
+
+function showPokemonSingle(i, pokemonName, pokemonImg, pokemonWeight, pokemontHeight){
+    let detailInterface = document.getElementById('detailInterface');
+    detailInterface.innerHTML += /*html*/ `
+       <div class="headInterface">
+           <div class="headInfo">
+                 <div class="nameID">
+                     <div class="idInterface">#${i}</div>
+                     <div class="nameInterface">${pokemonName}</div>
+                 </div>
+                 <div class="heightWeight">
+                     <div class="measure">Weight: ${pokemonWeight}</div>
+                     <div class="measure">Height: ${pokemontHeight}</div>
+                 </div>
+            </div>
+            <div class="imgInterfaceContainer"><img class="imgInterface" src="${pokemonImg}"></div>
+        </div>
+        `
+}
+
+
